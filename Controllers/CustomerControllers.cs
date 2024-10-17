4d20251082bc0b94b9fc71eef122368c06420b2a
Aryan <105101345+Maygodbless3u@users.noreply.github.com>
2024-10-17 16:13:02 +0545

    customer model update

--- Controllers/CustomerControllers.cs
+++ Controllers/CustomerControllers.cs
@@ -11,27 +11,6 @@ public class CustomerControllers : ControllerBase {
 
     [HttpGet]
     public IActionResult GetAll() {
-        var customer = _context.Customer.ToList();
-        return Ok(customer);
+        var listallCustomer = _context.Customer.ToList().Select(s => s.)
     }
-
-    [HttpGet("{id}")]
-    public IActionResult GetById([FromRoute] int id) {
-        var customer = _context.Customer.Find(id);
-        return Ok(customer);
-    }
-
-    [HttpGet("/register")] 
-    public IActionResult RedirectUser() {
-        return Redirect("http://localhost:5173/register");
-    }
-
-    [HttpPost("/login")]
-    public IActionResult Login ([FromBody] CustomerModel customerModel) {
-        if (customerModel.CustomerFirstName == "Aryan") {
-            return Ok(new {redirectUrl = "/login"});
-        } else {
-            return Unauthorized();
-        }
-    } 
 }
