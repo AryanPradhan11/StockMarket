1c98f6b526c61482cc86694a0d37b7239af301df
Aryan <105101345+Maygodbless3u@users.noreply.github.com>
2024-10-26 03:49:47 +0545

    Adding more contracts to update and delete

--- Interface/IStockRepo.cs
+++ Interface/IStockRepo.cs
@@ -1,3 +1,7 @@
 public interface IStockRepo {
     Task<List<User>> GetAllAsync();
+    Task<List<Stock>> GetStocksAsync();
+    Task<Stock?> CreateStockAsync(Stock stockmodel);
+    Task<Stock?> UpdateStockAsync(int id, UpdateStockDto updateDto);
+    Task<Stock?> DeleteStockAsync(int id);
 }
