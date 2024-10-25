59efac1df1897625e9900c33b6a809a0b7e064bf
Aryan <105101345+Maygodbless3u@users.noreply.github.com>
2024-10-26 03:49:11 +0545

    Injecting AddScoped for repositories

--- Program.cs
+++ Program.cs
@@ -17,6 +17,8 @@ builder.Services.AddDbContext<ApplicationDB>(options => {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddScoped<IStockRepo, UserRepository>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
