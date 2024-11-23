1bc283ecf34763b7607a4495d86d4f6c8c5dbdaf
Aryan <105101345+Maygodbless3u@users.noreply.github.com>
2024-11-23 11:00:33 +0545

    modified scopes

--- Program.cs
+++ Program.cs
@@ -21,6 +21,14 @@ builder.Services.AddScoped<IUserRepo, UserRepository>();
 builder.Services.AddScoped<IStockRepo, StockRepository>();
 builder.Services.AddScoped<ICommentRepo, CommentRepository>();
 
+
+
+builder.Services.AddCors(options => {
+    options.AddPolicy("AllowReactApp", 
+        policy => policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod()
+    );
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -29,6 +37,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("AllowReactApp");
 
 app.UseHttpsRedirection();
 
